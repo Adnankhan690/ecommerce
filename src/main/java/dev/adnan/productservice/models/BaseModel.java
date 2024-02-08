b9d261aad26080e63b094d9461203f302ace6d1b
Adnankhan690 <adnan73803@gmail.com>
2024-02-08 18:08:36 +0530

    Connected to a DB(MySQL), configured the properties and added the table to DB

--- src/main/java/dev/adnan/productservice/models/BaseModel.java
+++ src/main/java/dev/adnan/productservice/models/BaseModel.java
@@ -1,5 +1,18 @@
 package dev.adnan.productservice.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@MappedSuperclass
 public class BaseModel {
-    private Long id;
+    @Id
+//    @GeneratedValue(generator =  "uuidgenerator")
+//    @GenericGenerator(name = "uuidgenerator", strategy = "uuid4")
+//    @Column(name = "id", columnDefinition = "binary(16)", nullable = false, updatable = false)
+    private UUID id;
 }
--- src/main/java/dev/adnan/productservice/models/Category.java
+++ src/main/java/dev/adnan/productservice/models/Category.java
@@ -1,5 +1,8 @@
 package dev.adnan.productservice.models;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Category extends BaseModel {
     private String name;
 }
