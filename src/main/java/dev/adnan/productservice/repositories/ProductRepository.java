593e6836f07d987428198da7001274cd762e7e23
Adnankhan690 <adnan73803@gmail.com>
2024-02-09 18:18:20 +0530

    Implemented Repositories with functions,custom or native SQL querry and Hibernate Query Language(HQL) to talk to the Data Base

--- src/main/java/dev/adnan/productservice/repositories/ProductRepository.java
+++ src/main/java/dev/adnan/productservice/repositories/ProductRepository.java
@@ -2,9 +2,26 @@ package dev.adnan.productservice.repositories;
 
 import dev.adnan.productservice.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+    Product findByTitleEquals(String title);
+    Product findByTitleEqualsAndPrice_Price(String title, double price);
+    List<Product> findAllByPrice_Currency(String currency);
+
+    long countAllByPrice_Currency(String currency);
+
+    void delete(Product entity);
+    //Written in SQL, does not use Compile time check
+    @Query(value = CustomQueries.FIND_ALL_BY_TITLE,nativeQuery = true)
+    List<Product> findAllByTitle(String title);
+
+    //Written in HQL(Hibernate Query Language), uses Compile Time Check
+    @Query("select product from product where product.title = :title")
+    List<Product> readAllByTitle(String title);
 }
--- src/main/java/dev/adnan/productservice/services/SelfProductServiceImpl.java
+++ src/main/java/dev/adnan/productservice/services/SelfProductServiceImpl.java
@@ -1,24 +1,43 @@
-//package dev.adnan.productservice.services;
-//
-//import thirdPartyClients.productService.fakeStore.FakeStoreProductDTO;
-//import dev.adnan.productservice.DTO.GenericProductDTO;
-//import dev.adnan.productservice.models.Product;
-//import org.springframework.stereotype.Service;
-//
-//@Service("selfProductServiceImpl")
-//public class SelfProductServiceImpl implements ProductService {
-//    @Override
-//    public GenericProductDTO getProductById(Long id) {
-//        return new GenericProductDTO();
-//    }
-//
-//    @Override
-//    public GenericProductDTO createProduct(GenericProductDTO product) {
-//        return null;
-//    }
-//
-//    @Override
-//    public GenericProductDTO updateProductById(Long id) {
-//        return null;
-//    }
-//}
+package dev.adnan.productservice.services;
+
+import dev.adnan.productservice.DTO.GenericProductDTO;
+import dev.adnan.productservice.exceptions.NotFoundException;
+import dev.adnan.productservice.repositories.ProductRepository;
+import dev.adnan.productservice.thirdPartyClients.productservice.FakeStoreProductDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("selfProductServiceImpl")
+public class SelfProductServiceImpl implements ProductService {
+    private ProductRepository productRepository;
+
+    public SelfProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public GenericProductDTO getProductById(Long id) throws NotFoundException {
+        return null;
+    }
+
+    @Override
+    public GenericProductDTO createProduct(GenericProductDTO product) {
+        return null;
+    }
+
+    @Override
+    public GenericProductDTO updateProductById(FakeStoreProductDTO product, Long id) {
+        return null;
+    }
+
+    @Override
+    public GenericProductDTO deleteById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<GenericProductDTO> getAllProducts() {
+        return null;
+    }
+}
